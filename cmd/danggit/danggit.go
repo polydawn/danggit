c9febb2239ed0f05fb24012a43e55e2a455fcef8
Eric Myhre <hash@exultant.us>
2016-07-02 20:50:23 -0700

    Make some actual main method to play with.
    Though it's clear that I don't have any idea how this describe method is meant to be used... it does actually work, and is presumably doing something slightly nontrivial.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/danggit/danggit.go
+++ cmd/danggit/danggit.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/libgit2/git2go"
+)
+
+func main() {
+	repo, err := git.OpenRepository(".")
+	if err != nil {
+		fmt.Printf("not a repo: %s\n", err)
+		os.Exit(5)
+	}
+
+	desc, err := repo.DescribeWorkdir(&git.DescribeOptions{})
+	if err != nil {
+		fmt.Printf("madness: %s\n", err)
+		os.Exit(6)
+	}
+	fmt.Printf("repo: %s\n", desc)
+}
