6ad70e5e61219600b35b4287c52e6b8be71812a1
Eric Myhre <hash@exultant.us>
2016-07-07 22:16:27 -0500

    in my mind this is how i'd write a commit
    doesn't do quite what i'd expect, though.  looks like it's gonna be wise to back up again, indeed do some exec-wrapping of git, and make sure i know where my own feet are at.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- gat/heads_test.go
+++ gat/heads_test.go
@@ -2,6 +2,7 @@ package gat
 
 import (
 	"testing"
+	"time"
 
 	libgit "github.com/libgit2/git2go"
 	. "github.com/smartystreets/goconvey/convey"
@@ -24,12 +25,48 @@ func TestHeads(t *testing.T) {
 		})
 
 		Convey("and given some commits and branches", func() {
-			// TODO
-			_ = repo
+			// shove in file content
+			blobOid, err := repo.CreateBlobFromBuffer([]byte("hello, world!\n"))
+			maybePanic(err)
+
+			// assemble tree
+			treeBuilder, err := repo.TreeBuilder()
+			maybePanic(err)
+			defer treeBuilder.Free()
+			treeBuilder.Insert("thefile", blobOid, libgit.FilemodeBlob)
+			treeOid, err := treeBuilder.Write()
+			maybePanic(err)
+			// immediately look it up again because silly api
+			tree, err := repo.LookupTree(treeOid)
+			maybePanic(err)
+
+			// assemble commit
+			author := &libgit.Signature{
+				Name:  "author",
+				Email: "email@domain.wow",
+				When:  time.Date(2009, 10, 14, 12, 0, 0, 0, time.UTC),
+			}
+			commitOid, err := repo.CreateCommit(
+				"",
+				author,
+				author,
+				"log message",
+				tree,
+			)
+			maybePanic(err)
+			// immediately look it up again because silly api
+			commit, err := repo.LookupCommit(commitOid)
+			maybePanic(err)
+
+			// create branch
+			branch, err := repo.CreateBranch("branchname", commit, false)
+			maybePanic(err)
+			branch.Free()
 
 			Convey("ListHeads should work", func() {
 				resp := ListHeads(git.ReqListHeads{Repo: "repo"})
 				So(resp.Error, ShouldBeNil)
+				//So(resp.Heads, ShouldHaveLength, 1)
 			})
 		})
 	}))
