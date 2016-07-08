f3a4e6b8e15263d8184f82c4e3357e58145f9c5c
Eric Myhre <hash@exultant.us>
2016-07-07 20:44:43 -0500

    Attempt implement ls-remote equivalent on local repo.
    surprisingly massive difference in code path.
    
    doesn't entirely work because I have a not-entirely-thrilled eye towards the glob thing.  i feel this should have the same api as the actual remote function by the time we're done with it.  and there are todos about ref *types* for similar reasons.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- gat/heads.go
+++ gat/heads.go
@@ -6,14 +6,37 @@ import (
 	"polydawn.net/danggit/api"
 )
 
-func ListHeads(pth git.LocalRepoPath) []git.Head {
-	return nil // TODO
+func ListHeads(req git.ReqListHeads) git.RespListHeads {
+	// dial
+	repo, err := openRepository(req.Repo)
+	if err != nil {
+		return git.RespListHeads{Error: err}
+	}
+	// read heads
+	// FIXME why the hell does this take one glob when `remote.Ls` takes a filter *list*...
+	itr, err := repo.NewReferenceIteratorGlob("sightodo")
+	if err != nil {
+		return git.RespListHeads{Error: err}
+	}
+	// flip to our api types
+	heads := make([]git.Head, 0)
+	i := 0
+	for ref, err := itr.Next(); err == nil; ref, err = itr.Next() {
+		// TODO maybe check 'isBranch' and '!isRemote' here?  need to decide what semantics we really intend here
+		// FIXME ... which makes me realize, there's a reason heads and refs are different words; these funcs might not be using them entirely wisely
+		heads[i] = git.Head{
+			RefName:  ref.Name(),
+			CommitID: ref.Target().String(),
+		}
+		i++
+	}
+	return git.RespListHeads{Heads: heads}
 }
 
 func ListHeads_Remote(req git.ReqListHeadsRemote) git.RespListHeads {
 	remote := &libgit.Remote{}
 	// dial
-	err := remote.ConnectFetch(nil, nil, nil)
+	err := remote.ConnectFetch(nil, nil, nil) // FIXME wait, what?  where do i set the path?
 	if err != nil {
 		return git.RespListHeads{Error: err}
 	}
