c4477b951f7dbef97fb85030491cfe157ffec413
Eric Myhre <hash@exultant.us>
2016-07-17 15:20:16 -0500

    Simplify existing tests with the new functions.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- gat/heads_test.go
+++ gat/heads_test.go
@@ -24,44 +24,27 @@ func TestHeads(t *testing.T) {
 			})
 		})
 
+		// chunk of common fixture, not subject of interest
+		author := &git.CommitAttribution{
+			Name:  "author",
+			Email: "email@domain.wow",
+			When:  time.Date(2009, 10, 14, 12, 0, 0, 0, time.UTC),
+		}
+
 		Convey("and given some commits and branches", func() {
-			// shove in file content
-			blobOid, err := repo.CreateBlobFromBuffer([]byte("hello, world!\n"))
-			maybePanic(err)
-
-			// assemble tree
-			treeBuilder, err := repo.TreeBuilder()
-			maybePanic(err)
-			defer treeBuilder.Free()
-			treeBuilder.Insert("thefile", blobOid, libgit.FilemodeBlob)
-			treeOid, err := treeBuilder.Write()
-			maybePanic(err)
-			// immediately look it up again because silly api
-			tree, err := repo.LookupTree(treeOid)
-			maybePanic(err)
-
-			// assemble commit
-			author := &libgit.Signature{
-				Name:  "author",
-				Email: "email@domain.wow",
-				When:  time.Date(2009, 10, 14, 12, 0, 0, 0, time.UTC),
-			}
-			commitOid, err := repo.CreateCommit(
-				"",
-				author,
-				author,
-				"log message",
-				tree,
-			)
-			maybePanic(err)
-			// immediately look it up again because silly api
-			commit, err := repo.LookupCommit(commitOid)
-			maybePanic(err)
+			commitID := createCommit(repo, &git.Commit{
+				Author:    author,
+				Committer: author,
+				Message:   "log message",
+				Parents:   nil,
+			}, []treeEntry{
+				{Filename: "thefile", Filemode: libgit.FilemodeBlob, Content: []byte("hello, world!\n")},
+			})
+			expectedCommitID := "5409e1f57cf0ffe7a542e78a1c69ae715f2d2abc"
+			So(string(commitID), ShouldResemble, expectedCommitID)
 
 			// create branch
-			branch, err := repo.CreateBranch("branchname", commit, false)
-			maybePanic(err)
-			branch.Free()
+			setBranch(repo, "branchname", commitID)
 
 			Convey("ListHeads should work", func() {
 				resp := ListHeads(git.ReqListHeads{Repo: "repo"})
